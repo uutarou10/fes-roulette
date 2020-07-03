06026d94615b78bb19be1c84d2380989a3edbd17
Kota Nonaka <kota_nonaka@fringe81.com>
2020-07-03 12:45:30 +0900

    できた

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  timerId = null;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      names: [],
+      currentIndex: 0,
+      textAreaInput: ""
+    }
+  }
+
+  onStart = () => {
+    this.timerId = setInterval(() => {
+      this.setState({
+        currentIndex: Math.floor(Math.random() * this.state.names.length)
+      });
+    }, 100);
+  }
+
+  onStop = () => {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+      this.timerId = null;
+    }
+  }
+
+  onInputNameTextArea = (e) => {
+    this.setState({textAreaInput: e.target.value});
+  }
+
+  onSubmitNames = () => {
+    const names = this.state.textAreaInput.split('\n');
+    this.setState({names});
+  }
+
+  render() {
+    if (this.state.names.length < 1) {
+      return (
+        <div>
+          <textarea onInput={this.onInputNameTextArea} />
+          <button onClick={this.onSubmitNames}>OK</button>
+        </div>
+      );
+    }
+
+    return (
+      <div className="container">
+        <div>
+          <div className="name">{this.state.names[this.state.currentIndex]}</div>
+          <div className="buttonContainer">
+            <button onClick={this.onStart}>Start!</button>
+            <button onClick={this.onStop}>Stop!</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
